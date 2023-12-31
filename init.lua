e917fbcbfd0397431d620c5426cb2ac1956d6480
Fabio E <faepi-tech@pm.me>
2023-12-31 11:39:36 +0100

    default treesitter install lang

--- init.lua
+++ init.lua
@@ -171,7 +171,7 @@ require('which-key').register({
 
 require('nvim-treesitter.configs').setup {
 	-- Add languages to be installed here that you want installed for treesitter
-	ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+	ensure_installed = { 'go', 'lua', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
 
 	-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
 	auto_install = true,
