6dcb944571be9548f85b30f62d7b73cf2b8e0c02
Fabio Esposito <faepi-tech@pm.me>
2024-01-03 10:04:22 +0100

    add cmp path and buffer

--- init.lua
+++ init.lua
@@ -66,6 +66,8 @@ require("lazy").setup({
 	{ 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
 	{ 'neovim/nvim-lspconfig' },
 	{ 'hrsh7th/cmp-nvim-lsp' },
+	{ 'hrsh7th/cmp-path' },
+	{ 'hrsh7th/cmp-buffer' },
 	{ 'hrsh7th/nvim-cmp' },
 	{ 'L3MON4D3/LuaSnip' },
 	{
