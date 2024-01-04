d30ad25809e69db5ff552f75d6454374c538aa2f
Fabio E <faepi-tech@pm.me>
2024-01-05 00:27:13 +0100

    change cmp keybindings

--- init.lua
+++ init.lua
@@ -210,6 +210,13 @@ cmp.setup({
 		{ name = 'buffer',  keyword_length = 3 },
 	},
 	formatting = lsp_zero.cmp_format(),
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
 })
 
 -- see :help lsp-zero-guide:integrate-with-mason-nvim
