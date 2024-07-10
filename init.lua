076456fd21261291ee013f8913f41165ad578ccc
Fabio E <fabio.espositopires@goflink.com>
2024-07-10 19:21:50 +0200

    better keybindings for dap

--- init.lua
+++ init.lua
@@ -111,20 +111,17 @@ require("lazy").setup({
 			require("dapui").setup()
 			require("dap-go").setup()
 
-			vim.keymap.set("n", "<F5>", dap.toggle_breakpoint)
-			-- vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
-
 			-- Eval var under cursor
 			vim.keymap.set("n", "<space>?", function()
 				require("dapui").eval(nil, { enter = true })
 			end)
 
+			vim.keymap.set("n", "<F5>", dap.toggle_breakpoint)
 			vim.keymap.set("n", "<F6>", dap.continue)
-			vim.keymap.set("n", "<F7>", dap.step_into)
-			vim.keymap.set("n", "<F8>", dap.step_over)
-			vim.keymap.set("n", "<F9>", dap.step_out)
-			vim.keymap.set("n", "<F10>", dap.step_back)
-			vim.keymap.set("n", "<F12>", dap.restart)
+			vim.keymap.set("n", "<F7>", dap.restart)
+			vim.keymap.set("n", "<F8>", dap.step_into)
+			vim.keymap.set("n", "<F9>", dap.step_over)
+			vim.keymap.set("n", "<F10>", dap.step_out)
 
 			dap.listeners.before.attach.dapui_config = function()
 				ui.open()
